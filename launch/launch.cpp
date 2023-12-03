d3536290940776203a60a2d0ab29f351304313bb
furumori <73024620+koumori8@users.noreply.github.com>
2023-12-03 18:03:05 +0900

    Add files via upload

---
+++ launch/launch.cpp
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='joy',
+            executable='joy_node',
+        ),
+        Node(
+            package='twister',
+            executable='launch',
+        ),
+        Node(
+            package='ver',
+            executable='launch',
+        )
+    ])
